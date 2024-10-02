c325c4e5e30c638bc06037d3983a606e8da114a8
Rinzler <119778544+TheRinzler65@users.noreply.github.com>
2024-10-02 22:22:07 +0200

    update batch file

--- README.md
+++ README.md
@@ -1,28 +1,19 @@
-# Convertisseur .3ds à .cia
-Un convertisseur de 3DS en CIA pour Windows (Linux pas sûr à 100%).
+# Multitool 3DS
+Un ensemble d'outils destiné pour la Nintendo 3DS
 
 ## Utilisation
-### Facile (version précompilée)
-Décompressez simplement la version publiée, placez vos ROMs dans le répertoire `roms`, vos xorpads dans le répertoire `xorpads`, puis lancez `Convertisseur .3ds à .cia`.
-Le script vous guidera à travers les étapes nécessaires.
-Les fichiers CIA résultants seront situés dans le répertoire `cia`.
 
-### Pro (à partir des sources)
-- Installez une version 3.x.x de python
+#### Sur Windows :
 
-- Le programme `make_cia` est déjà dans le code source où alors installez le et mettez le dans votre `PATH` (Les deux marchent)
+Vous devez lancer le fichier `multitool_3ds.bat` et vous verrez une page s'afficher avec différent point sur le menu : 
 
-## Compilation de la version
-### Sur Windows :
+- 1. Convertisseur .3ds / .cia : ça permet de convertir les rom decrypter et certaines rom encrypter (grâce aux xorpads (fichier signature)) .3ds en .cia, lorsque c'est terminer, vous le retrouverez dans le dossier `cia`.
 
-Vous devez installer python 3.x.x avec pyinstaller et colorama (normalement par défaut), puis faites dans un Terminal :
-```
-pyinstaller 3ds-to-cia.spec
-```
+- 2. Convertisseur .cia / .3ds : ça permet de convertir les roms decrypter .cia en rom decrypter .3ds, il faut mettre la rom à la racine du dossier (au même endroit que `multitool_3ds.bat`), lorsque c'est terminer, vous le retrouverez dans le dossier `3ds`.
 
-Dans le répertoire `dist` vous trouverez le fichier binaire généré.
+- 3. Rejoindre notre Serveur Discord : Si jamais vous avez des problèmes par rapport au convertisseur ou un problème après conversion du fichier, veuillez faire un post sur notre serveur discord on vous répondra le plus vite possible.
 
-Placez le fichier binaire dans un dossier avec les répertoires vides  `cia`, `roms` et `xorpads` compressez-le et redistribuez-le.
+- 4. 
 
 
 ### Sur Linux : (prochainement)
---
+++ multitool_3ds.bat
@@ -0,0 +1,87 @@
+@echo off
+chcp 65001 >nul
+goto banner
+
+:banner
+:menu
+echo.
+echo.
+echo            ███╗   ███╗██╗   ██╗██╗  ████████╗██╗████████╗ ██████╗  ██████╗ ██╗         ██████╗ ██████╗ ███████╗
+echo            ████╗ ████║██║   ██║██║  ╚══██╔══╝██║╚══██╔══╝██╔═══██╗██╔═══██╗██║         ╚════██╗██╔══██╗██╔════╝
+echo            ██╔████╔██║██║   ██║██║     ██║   ██║   ██║   ██║   ██║██║   ██║██║          █████╔╝██║  ██║███████╗
+echo            ██║╚██╔╝██║██║   ██║██║     ██║   ██║   ██║   ██║   ██║██║   ██║██║          ╚═══██╗██║  ██║╚════██║
+echo            ██║ ╚═╝ ██║╚██████╔╝███████╗██║   ██║   ██║   ╚██████╔╝╚██████╔╝███████╗    ██████╔╝██████╔╝███████║            
+echo.
+echo.
+
+echo                                        Menu Principal:
+echo                                       -----------------
+echo.
+echo.
+echo                                1. Convertisseur .3ds / .cia
+echo.
+echo                                2. Convertisseur .cia / .3ds (à venir)
+echo.
+echo                                3. Rejoindre notre serveur Discord
+echo.
+echo                                4. Quitter
+echo.
+
+set /p choix="Choisissez une option : "
+
+if "%choix%"=="1" goto convertisseur_3ds_cia
+if "%choix%"=="2" goto cia-to-3ds
+if "%choix%"=="3" goto discord
+if "%choix%"=="4" goto quitter
+goto menu
+
+:convertisseur_3ds_cia
+python convertisseur_3ds_cia.py
+cls
+goto menu
+
+:cia-to-3ds
+@ECHO OFF
+cd %~dp0
+
+:: Détermine l'architecture du système (64 bits ou 32 bits)
+IF "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+    SET makerom_path=tools\win64\makerom.exe
+) ELSE (
+    SET makerom_path=tools\win32\makerom.exe
+)
+
+:: Vérifie si makerom.exe existe dans le chemin déterminé
+IF NOT EXIST "%makerom_path%" (
+    ECHO makerom.exe introuvable dans tools\win64 ou tools\win32.
+    EXIT /B 1
+)
+
+:: Crée le dossier .3ds s'il n'existe pas
+IF NOT EXIST ".3ds" (
+    mkdir .3ds
+)
+
+:: Boucle pour traiter tous les fichiers .cia dans la racine du projet
+FOR %%F IN (*.cia) DO (
+    ECHO Conversion de %%F...
+    "%makerom_path%" -ciatocci "%%F"
+    ren "%%~dpnF.cci" "%%~nF.3ds"
+    move "%%~nF.3ds" ".3ds\"
+    ECHO %%F converti et déplacé dans .3ds
+)
+
+ECHO Conversion terminée. Tous les fichiers .cia ont été convertis et déplacés dans le dossier '.3ds'
+cls
+goto menu
+
+:discord
+
+pause
+cls
+goto menu
+
+:quitter
+echo Au revoir !
+pause
+exit
